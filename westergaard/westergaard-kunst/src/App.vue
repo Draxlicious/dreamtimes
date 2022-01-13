c4257a8a5a6cb501bb4b94e396eb15599f3db8cd
Lukas Mark Jakobsen <lukasjakobsen17@hotmail.com>
2022-01-13 15:01:25 +0100

    init westergaard

---
+++ westergaard/westergaard-kunst/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <head>
+    <link rel="stylesheet" href="./bootstrap-5.0.2/dist.css/bootstrap.css">
+  </head>
+  <body>
+    <header-component></header-component>
+    
+  </body>
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body{
+  margin:0;
+  height:100vh;
+}
+</style>
---
+++ westergaard/westergaard-kunst/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <nav>
+            <ul class="ul--element">
+                <a href=""><li>HOME</li></a>
+                <a href=""><li>SHOP</li></a>
+                <a href=""><li>NEWS</li></a>
+                <a href=""><li>PRESS</li></a>
+                <a href=""><li>CONTACT</li></a>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+
+    header{
+        background-color:#222;
+        color:#fff;
+        padding:1em;
+
+    }
+
+    .ul--element{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        list-style: none;
+        margin:0 auto;
+        min-width: 15em;
+        font-size: 0.8em;
+    }
+    ul a{
+        color: #fff;
+        text-decoration: none;
+    }
+
+    ul a li {
+        padding: 1em;
+    }
+</style>
