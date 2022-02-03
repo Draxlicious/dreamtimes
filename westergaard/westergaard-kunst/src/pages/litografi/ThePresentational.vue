60693b5560783c2a30ea5955ffcd7ffe4e5b8394
Lukas Mark Jakobsen <lukasjakobsen17@hotmail.com>
2022-02-03 15:29:57 +0100

    images added to presentational
    Fixed bugg to load images. Change JPG to jpg

--- westergaard/westergaard-kunst/src/pages/litografi/ThePresentational.vue
+++ westergaard/westergaard-kunst/src/pages/litografi/ThePresentational.vue
@@ -1,3 +1,32 @@
 <template>
     Presentational
+    <main class="main-container">
+        <section class="grid-section">
+            <img :src="require(`../../assets/static/${image}`)" alt="">
+        </section>
+    </main>
 </template>
+
+<style scoped>
+.grid-section{
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+
+img{
+    height:500px;
+    width: 500px;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            
+            image: 'IMG_3920.jpg'
+           
+        }
+    }
+}
+</script>
