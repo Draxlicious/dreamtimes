82600580ddd4e7e2315fad78a6e06b88b00c6a5b
Lukas Mark Jakobsen <lukasjakobsen17@hotmail.com>
2022-02-16 15:02:48 +0100

    new images from customer (changes)

--- westergaard/westergaard-kunst/src/pages/litografi/TheContact.vue
+++ westergaard/westergaard-kunst/src/pages/litografi/TheContact.vue
@@ -1,50 +1,59 @@
 <template>
     <main class="main-container">
         <div class="d-flex">
-            <img src="https://picsum.photos/500/600" alt="">
             <div class="information-details">
-                <h2 class="contact-title">Dream times v. Palle Westergaard</h2>
-                <p>Kontakt information samt kontakt format til meddelelser: Palle Westergaard</p>
+                <h2 class="contact-title">Dream times - Palle Westergaard</h2>
+                <p>www.westergaards.site</p>
+                <p>Instagram: pw_kunst</p>
                 <p>Frederiksberg C.</p>
+                <p>+ 45 20868462 (opringning, besked og SMS).</p>
                 <p>E-mail: pallewestergaard@hotmail.com</p>
-                <p>Mob.:  0045 20868462.</p>
+                <p>CVR 37288586</p>
             </div>
+            <img :src="images.imagePath" alt="">
         </div>
     </main>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+            images: {
+                    imagePath: require(`../../assets/static/Forside_5.jpg`)
+                },
+        }
+    }
+}
+</script>
 
 <style scoped>
 img{
     width:100%;
     height: auto;
+    margin-top: 1em;
 }
 .d-flex{
     /* justify-content: space-between; */
     align-items: center;
     text-align: left;
+    flex-direction: column;
 }
 .information-details{
-    padding:0 2em;
+    /* padding-top: 2em;
+    padding-left: 1.5em; */
     color: #333;
 }
-
 .contact-title{
     font-weight: 400;
     color: #6a6262;
 }
-
-@media only screen and (max-width: 700px) {
-  .d-flex {
-    flex-direction: column;
-  }
-  .information-details{
-      padding-top: 2em;
-      padding-left: 1.5em;
-    }
-}
-@media only screen and (min-width: 1024px) {
+@media only screen and (min-width: 1250px) {
   .information-details{
       font-size: 1.5em;
+  }
+  .main-container{
+       padding: 2em 15%;
   }
 }
 </style>
